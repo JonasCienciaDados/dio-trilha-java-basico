fda2f913c4ebad64fc1e13f845d4b3cd4ab14e6d
Jonas Tereza <jonas.tereza1985@gmail.com>
2023-02-10 18:57:52 -0300

    Projeto Liga/Desliga SmartTv

---
+++ tipo-variavel/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+
+        
+
+
+    }
+        
+    
+}
--- tipo-variavel/src/TiposVariavel.java
+++ tipo-variavel/src/TiposVariavel.java
@@ -1,5 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String [] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
     }
 }
